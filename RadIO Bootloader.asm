db4677143384fd65ec54bd7834824041eb7ab563
ehrenjn <ehrenjulienneitzert@cmail.carleton.ca>
2019-09-23 20:07:32 -0400

    commiting some comments before I delete em

--- RadIO Bootloader.asm
+++ RadIO Bootloader.asm
@@ -11,18 +11,30 @@
 	;also makes interrupts unable to be invoked from bootloader (which we're disabling anyway)
 	;also makes application code unable to read bootloader code
 
-;AHH NOT CHECKING IF THERES NO MORE BYTES BEING SENT ON USART
-;GOTTA FIGURE OUT FRAME FORMAT FOR SERIAL COMM (dont even think I can use avrdude)
-	;if we do end up using a standalone script then the bootloader could send back info about fuses and stuff
-;DO I CARE ABOUT INITAL STATE OF GPIO (would it be bad to have things as input by default? I guess not since its what the atmega does anyway but seems weird to me)
-	;ask noid
 ;MIGHT WANNA #DEFINE SOME BETTER NAMES FOR THE REGISTERS (since most of them stay constant)
 ;MAKE SURE YOU'RE READING THE RIGHT PIN
 ;ADD ERROR HANDLING (send an error byte and spin forever)
 ;MAKE SURE YOU INIT EVERYTHING YOU NEED TO (BASICALLY clr EVERYTHING)
-;STILL HAVE NO WAY OF SKIPPING RECV WORD WHEN QUEUE IS FULL (should be ez though)
 ;HOW LONG DOES IT TAKE TO DO SPM FOR BUFFER FILLING?? COULD I JUST SPAM BUFFER FILL SPM????
 ;IS THE SRAM 2048 BYTES OR 2000? PROBABLY 2048 TBH
+;MAKE A VAR TO KEEP TRACK OF WHAT WORD YOU'RE ON (R2) thats only used for adding to buffer
+;OH MAN I SHOULD JUST STORE A QUEUE SIZE VARIABLE AND THEN I WOULDNT HAVE TO DEAL WITH THE NASTY QUEUE OPs
+	;to check if queue is empty you would just OR both current queue size bytes and check if it's 0
+	;which is only 1 byte faster than doing a cpc...
+	;and only shave off like 5 instructions from checking if queue is full (still need to do a 16 bit cp)
+	;and you lose 2 instructions to incrementing and loweing the current queue size every round
+	;so all in all its not really worth it, just figure out cpc
+;WOULD BE FASTER IF I REQUESTED THE NEXT WORD BEFORE ADDING THE CURRENT WORD TO THE QUEUE BUT IT WOULD BE HARD TO IMPLEMENT (because I'd have to increase queue by 4 with looping)
+;DONT THINK NUM_PAGES WILL EVER ACTUALLY GET USED ANYWHERE
+;MAKE SURE YOU INCREASE Z, CLEAR THE BUFFER SIZE, AND CLEAR THE HAS BEEN ERASED BIT AFTER YOU WRITE THE PAGE
+	;also make sure you set the erased flag at the end of a page erase
+;IN THE FIRST TWO JUMPS WHEN IM RECIVING A WORD IM NOT SURE IF I SHOULD BE JUMPING TO CHECKING IF THE QUEUE IS FULL OR JUMP RIGHT TO FLASH WRITING, FIGURE IT OUT
+	;the first one is definately wrong (or at least, the comment is)
+	;might need to delete check_if_queue_full just because nothing ever jumps to it
+;IVE SO FAR GOT NO WAY TO WRITE A PARTIALLY FILLED BUFFER TO THE FLASH
+	;hint: right now you're not doing ANY spm if theres no data in the queue... but the only spm operation that requires the queue is filling the buffer!
+;KIDNA NASTY THAT I NEED TO SUBTRACT PAGE_LENGTH (or just 1 because thatd work too) WHENEVER I ERASE OR WRITE FLASH, BUT I DONT THINK THERES A BETTER WAY 
+	;only the word bits are used when writing to temp buff and only page bits are used for erase/write... so maybe you can figure something out
 ;EVERYTHING IS UNTESTED (of course)
 
 ;go to page 277 in atmega datasheet to read about "programming the flash"
@@ -40,9 +52,6 @@
 
 
 .INCLUDE iodefs.asm
-
-.EQU PAGE_LENGTH = 64
-.EQU NUM_PAGES = 252 ;not 256 because last 4 pages are the bootloader itself
 
 ;gpio pins
 .EQU BTN_IN_PIN = 0 ;pin that bootloader button is on
@@ -62,10 +71,16 @@
 .DEF TAIL_LO = 
 .DEF TAIL_HI =
 
+;definitions for spm operations
+.EQU PAGE_LENGTH = 64
+.EQU NUM_PAGES = 252 ;not 256 because last 4 pages are the bootloader itself
+.DEF CURRENT_PAGE_BUFFER_SIZE = R2 ;keeps track of how many words we've inserted into the page buffer 
+.DEF PAGE_HAS_BEEN_ERASED = R3 ;1 if the current page has been erased, 0 otherwise
+
 ;other register definitions
-.DEF CURRENT_WORD_ADDR = Z ;address to place the current word we're inserting into the page buffer
-.DEF CURRENT_WORD_ADDR_LO = R30
-.DEF CURRENT_WORD_ADDR_HI = R31
+.DEF LAST_BUFFERED_WORD_ADDR = Z ;address to place the LAST word that WAS inserted into the page buffer (last word instead of current word because I'm incrementing this one register to do all buffering/erasing/writing which means I need it to be 63 when I'm erasing/writing the 0th page instead of 64)
+.DEF LAST_BUFFERED_WORD_LO = R30
+.DEF LAST_BUFFERED_WORD_HI = R31
 .DEF CURRENT_WORD_LO = R0 ;when adding a word to the page buffer we have to address it using R1:R0 
 .DEF CURRENT_WORD_HI = R1
 
@@ -92,15 +107,15 @@
 	cbi DDRB, BTN_IN_PIN ;set the proper pin as input (should be default behavior anyway)
 	nop ;must wait one clock cycle for input register to update after setting pins as input
 	sbic PINB, BTN_IN_PIN ;if pin is high (button is pressed) then enter the bootloader
-	rjmp end_flash_write ;exit bootloader if button is not pressed
+	rjmp exit_bootloader ;exit bootloader if button is not pressed
 
 ;turn on LED to indicate we're in the bootloader
 	sbi DDRB, LED_PIN ;set LED_PIN as output
 	sbi PORTB, LED_PIN ;turn LED on
 
 ;set up variables for program loading
-	clr CURRENT_WORD_ADDR_LO
-	clr CURRENT_WORD_ADDR_HI ;current word in flash that we're updating = 0
+	set LAST_BUFFERED_WORD_LO ;set instead of clr because it needs to be 0 after the first word is added to the buffer
+	set LAST_BUFFERED_WORD_HI ;last word that was put in the page buffer was -1
 	clr R3 ;current page number = 0
 	clr DONE_RECEIVING_DATA ;not done receiving data
 
@@ -147,30 +162,17 @@ wait_for_byte_2:
 ;read received data, no need to check bit 9 this time
 	in CURRENT_WORD_LO, UDR0 ;read byte from USART
 
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                add word to queue
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;add word to queue (we already know that queue is not full and that it is not pointing outside the queue)
 	st HEAD+, CURRENT_WORD_HI ;store first byte of received word in queue and move the head forward
 	st HEAD+, CURRENT_WORD_LO ;also store 2nd byte and increment
 
-;check if head should be looped back to 0
-	cpi HEAD_LO, lo_byte QUEUE_SIZE ;check if lo byte of head indicates it might be at the end of the queue
-	brne check_if_queue_full ;if lo byte of head can't be at end of queue, start doing flash stuff
-	cpi HEAD_HI, hi_byte QUEUE_SIZE ;check if head is at end of queue
-	brne check_if_queue_full ;if we're not at the end of the queue yet start doing flash stuff
-
-;loop head back to beginning of queue
-	clr HEAD_HI
-	clr HEAD_LO
+;reset head to 0 if it has reached the end of the queue
+	clear_word_if_equal HEAD_HI, HEAD_LO, QUEUE_END
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                request next word
+;                               request next word
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 check_if_queue_full:
@@ -182,11 +184,13 @@ check_if_queue_full:
 	movw GENERAL_PURPOSE_WORD_REG, HEAD ;copy head
 	adiw GENERAL_PURPOSE_WORD_REG, 2 ;increase by 2
 
-;check if HEAD is 2 bytes away from TAIL (queue is full)
+;loop back to 0 if HEAD+2 reaches the end of the queue
+	clear_word_if_equal GENERAL_PURPOSE_REG_2, GENERAL_PURPOSE_REG_1, QUEUE_END
+
+;check if (HEAD + 2) % QUEUE_END == TAIL
 	cp GENERAL_PURPOSE_REG_1, TAIL_LO ;check lo byte of incremented head copy
-	brne check_if_queue_empty ;if lo byte of head can't be at end of queue, start trying to do flash stuff
-	cp GENERAL_PURPOSE_REG_2, TAIL_HI ;check if head is at end of queue
-	brne check_if_queue_empty ;if we're not at the end of the queue yet start trying to do flash stuff
+	cpc GENERAL_PURPOSE_REG_2, TAIL_HI ;check if head is at end of queue (cpc instruction works in this context because it only sets the z flag if z was already 1)
+	breq check_if_queue_empty ;queue is full, lets do flash stuff but without requesting another byte
 
 ;the queue is not full, so we can request another word from the sender
 	send_byte REQUEST_NEXT_WORD
@@ -195,17 +199,63 @@ check_if_queue_full:
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;                                  update flash
+;                         figure out how to update flash
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 check_if_queue_empty:
 	cp HEAD_LO, TAIL_LO ;yet another 16 bit compare, this time checking if HEAD == TAIL
-	brne CHECK NEXT THING ;if tail != head then we can move on to checking what kind of spm instruction we should do
-	cp HEAD_HI, TAIL_HI
-	brne CHECK NEXT THING 
+	cpc HEAD_HI, TAIL_HI ;compare with carry (so that result of lo byte cp is taken into account)
+	breq receive_word ;the queue is empty so we need to get another word in there (can't update flash with no queued words) 
 
-;queue is empty (TAIL == HEAD)
-	rjmp receive_word ;get another word in the queue (can't update flash with no queued words) 
+;check if we can do spm
+	sbic SPMCSR, 0 ;check if spm is still going, skip next instruction if it's not
+	rjmp receive_word ;we can't do any spm so get the next word instead
+
+;check if page buffer is full yet
+	cpi CURRENT_PAGE_BUFFER_SIZE, PAGE_LENGTH 
+	brne fill_page_buffer ;if page buffer isn't full then we have to keep filling it up before doing other spm
+
+;check if we've erased the current page yet
+	cpi PAGE_HAS_BEEN_ERASED, 1 ;check if page has been erased
+	brne erase_page ;erase page if it hasn't been erased yet
+
+;write the page (since we've already done everything else)
+	ldi R2, 0b00000101 ;enable SPM, write mode
+	out SPMCSR, R2
+	spm ;write the page
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                              fill the page buffer
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+fill_page_buffer:
+
+;dequeue latest word
+	ld CURRENT_WORD_HI, TAIL+ ;dequeue first byte and move tail forward
+	ld CURRENT_WORD_LO, TAIL+ ;dequeue 2nd byte and move tail forward
+
+;loop tail back to beginning of queue if need be
+	clear_word_if_equal TAIL_HI, TAIL_LO, QUEUE_END
+
+;increase word counts
+	inc CURRENT_PAGE_BUFFER_SIZE ;track the size of the page buffer
+	adiw LAST_BUFFERED_WORD, 1 ;increase this BEFORE adding to the buffer because spm instruction needs the address for the current word in Z to work
+
+;add word to page buffer
+	sbi SPMCSR, 0 ;enable SPM, page buffer fill mode
+	spm ;add word to temporary page buffer
+
+;restart main loop
+	rjmp receive_word
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                  update flash
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 update_next_page:
 	ld R2, 0 ;current word number
@@ -254,9 +304,9 @@ wait_write_complete:
 
 ;check if bootloading is done
 	cpi R5, 1 ;check if last serial byte was read successfully 
-	breq end_flash_write ;exit if last serial read was unsuccessful (all data has been transfered)
+	breq exit_bootloader ;exit if last serial read was unsuccessful (all data has been transfered)
 	cpi R3, NUM_PAGES ;check if we've written all flash except the bootloader
-	brsh end_flash_write ;exit if max page number is reached
+	brsh exit_bootloader ;exit if max page number is reached
 
 ;write another page if bootloading is not done	
 	inc R3 ;otherwise, write another page
@@ -265,7 +315,7 @@ wait_write_complete:
 
 
 
-end_flash_write:
+exit_bootloader:
 	cbi PORTB, LED_PIN ;turn off LED to indicate we're no longer in the bootloader
 	jmp 0 ;start uploaded program
 
@@ -306,4 +356,26 @@ wait_for_empty_transmit_buffer:
 ;return: hi byte of input
 .MACRO hi_byte:
 	(@0 >> 8)
+.ENDMACRO
+
+
+
+;args: 	0: register containing hi byte of input word
+;		1: register containing lo byte of input word
+;		2: immediate word to compare @0:@1 to
+;return: if @0:@1 == @2 then @0:@1 is cleared to 0. otherwise, nothing happens
+.MACRO clear_word_if_equal:
+
+;check if input word should be cleared
+	cpi @1, lo_byte @2 ;check lo byte of input registers
+	brne return\@: ;return if lo byte register differs from comparison word
+	cpi @0, hi_byte @2 ;can't use cpc instruction because I'm comparing to an immediate 
+	brne return\@: ;words aren't the same, return
+
+;clear the input registers
+	clr @0
+	clr @1
+
+return\@: ;looks weird because I have to make every use of this macro spit out a unique label here
+
 .ENDMACRO
